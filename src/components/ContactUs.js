ff5d8ab0aea73443245295ef7f67e47206e73011
sourav <kumarsourav7012@gmail.com>
2024-07-25 15:36:40 +0530

    school-web

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -56,6 +56,7 @@ const ContactUs = () => {
                         <div className="map-embed">
                             <iframe 
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12688.527691090048!2d-74.00594129999999!3d40.71277569999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a17ef15a305%3A0xd5f6bfcfa058b2d!2sSpringdale%20Public%20School!5e0!3m2!1sen!2sus!4v1635429327597!5m2!1sen!2sus"
+                                title="frame"
                                 width="100%" 
                                 height="300" 
                                 style={{ border: 0 }} 
